8a3b2a4e0c6826ed81991a14a24e56aee5d1b051
Precious Davies <92186079+Preshique@users.noreply.github.com>
2021-11-26 19:33:28 +0100

    Add files via upload

---
+++ project.js
@@ -0,0 +1,80 @@
+   
+      
+      const form = document.getElementById('form');
+      form.addEventListener('submit',(e)=> {
+        e.preventDefault();
+             location.replace('Project4.html')
+          
+      })
+      window.load=doShowAll();
+      var slideshows = document.querySelectorAll('[data-component="slideshow"]');
+slideshows.forEach(initSlideShow);
+
+/**function initSlideShow(slideshow) {
+
+	var slides = document.querySelectorAll(`#${slideshow.id} [role="list"] .slide`);
+
+	var index = 0, time = 5000;
+	slides[index].classList.add('active');
+
+	setInterval( () => {
+		slides[index].classList.remove('active');
+		
+		index++;
+		if (index === slides.length) index = 0;
+
+		slides[index].classList.add('active');
+
+	}, time);
+}**/
+
+var slideIndex = 1;
+    showSlides(slideIndex);
+    
+    function plusSlides(n) {
+      showSlides(slideIndex += n);
+    }
+    
+    function currentSlide(n) {
+      showSlides(slideIndex = n);
+    }
+    
+    function showSlides(n) {
+      var i;
+      var slides = document.getElementsByClassName("mySlides");
+      var dots = document.getElementsByClassName("dot");
+      if (n > slides.length) {slideIndex = 1}    
+      if (n < 1) {slideIndex = slides.length}
+      for (i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none";  
+      }
+      for (i = 0; i < dots.length; i++) {
+          dots[i].className = dots[i].className.replace(" active", "");
+      }
+      slides[slideIndex-1].style.display = "block";  
+      dots[slideIndex-1].className += " active";
+    }
+// Dynamically populate the table with shopping list items.
+//Step below can be done via PHP and AJAX, too.
+/*function doShowAll() {
+    if (CheckBrowser()) {
+        var key = "";
+        var list = "<tr><th>Item</th><th>Value</th></tr>\n";
+        var i = 0;
+        //For a more advanced feature, you can set a cap on max items in the cart.
+        for (i = 0; i <= localStorage.length-1; i++) {
+            key = localStorage.key(i);
+            list += "<tr><td>" + key + "</td>\n<td>"
+                    + localStorage.getItem(key) + "</td></tr>\n";
+        }
+        //If no item exists in the cart.
+        if (list == "<tr><th>Item</th><th>Value</th></tr>\n") {
+            list += "<tr><td><i>empty</i></td>\n<td><i>empty</i></td></tr>\n";
+        }
+        //Bind the data to HTML table.
+        //You can use jQuery, too.
+        document.getElementById('list').innerHTML = list;
+    } else {
+        alert('Cannot save shopping list as your browser does not support HTML 5');
+    }
+}
